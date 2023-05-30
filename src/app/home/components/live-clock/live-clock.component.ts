553538d9bff4ee021bc28590c05ae90c134efa41
luisr160 <luis.rosero1@utp.ac.pa>
2023-05-30 16:59:29 -0500

    provisional changes

--- src/app/home/components/live-clock/live-clock.component.ts
+++ src/app/home/components/live-clock/live-clock.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import {format} from 'date-fns';
+import {  Component, OnInit } from '@angular/core';
+import { LiveDateService } from '../../services/live-date.service';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-live-clock',
   templateUrl: './live-clock.component.html',
   styleUrls: ['./live-clock.component.css']
 })
-export class LiveClockComponent implements OnInit {
+export class LiveClockComponent implements OnInit{
 
-  public algo = format(new Date(2014, 1, 11), 'MM/dd/yyyy');
+  public currentTime:string = '';
+  public dateArray!:string[];
 
   constructor() { }
 
+  // get liveTime():string[]{
+  //   return this.liveTimeService.dateArray;
+  // }
+
+  private async updateCurrentTime(){
+    const now = new Date();
+    this.currentTime = format(now,'d LLL - hh:mm aa');
+    this.dateArray = this.currentTime.split('-');
+    //console.log(this.dateArray)
+  }
+
   ngOnInit(): void {
+    setInterval(()=>{
+      this.updateCurrentTime();
+    }, 1000);
   }
 
 }
