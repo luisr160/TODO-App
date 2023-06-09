63ae5aba5f85e10b4f6e8574d0c6237334cc6133
luisr160 <luis.rosero1@utp.ac.pa>
2023-06-09 12:25:59 -0500

    prueba splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,16 @@ const config: CapacitorConfig = {
   webDir: 'www',
   server: {
     androidScheme: 'https'
-  }
+  },
+  plugins: {
+    SplashScreen: {
+        launchShowDuration: 3000,
+        launchAutoHide: true,
+        androidScaleType: "CENTER_CROP",
+        splashImmersive: true,
+        backgroundColor: "#ffffff"
+    }
+}
 };
 
 export default config;
